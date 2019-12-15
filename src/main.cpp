f7c28108528266f236405b3ad1067fa8467b39f8
diego <tembio@gmail.com>
2019-12-15 20:53:37 +0000

    Add main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+#include <stdexcept>
+#include <iostream>
+#include <fstream>
 #include "argParser.h"
+#include "jobScheduler.h"
+#include "task.h"
+
 
 int main(int argc, char* argv[]){
-	Scheduler::ArgParser parser(argc, argv);
+	try{
+		Scheduler::ArgParser parser(argc, argv);
+		Scheduler::JobScheduler scheduler;
+
+		for(auto taskFile : parser.taskFiles()){
+			std::ifstream file(taskFile);
+			Scheduler::Task task(file);
+			file.close();
+
+			scheduler.addJob(task);
+		}
+
+		scheduler.run(parser.taskToExecute());
+
+	}catch(const std::runtime_error& e){
+		std::cout<<e.what()<<"\n";
+	}
 }
